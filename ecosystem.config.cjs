6f2b3390f29c432e7d16ac1bfd3bd8daaf554509
Tomer Neeman <tomerneeman@Tomers-MacBook-Pro.local>
2026-02-06 12:21:35 +0200

    update ci

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
     apps: [
         {
-            name: 'taskpoint',
+            name: 'taskpoint-server',
             script: './server/index.js',
-            error_file: './logs/err.log',
-            out_file: './logs/out.log',
+            error_file: './logs/server-err.log',
+            out_file: './logs/server-out.log',
             log_date_format: 'YYYY-MM-DD HH:mm:ss',
             instances: 1,
             autorestart: true,
@@ -14,6 +14,22 @@ module.exports = {
                 NODE_ENV: 'production',
                 PORT: 3000
             }
+        },
+        {
+            name: 'taskpoint-frontend',
+            script: 'npm',
+            args: 'run preview',
+            error_file: './logs/frontend-err.log',
+            out_file: './logs/frontend-out.log',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            env: {
+                NODE_ENV: 'production',
+                PORT: 4173
+            }
         }
     ]
 };
+
