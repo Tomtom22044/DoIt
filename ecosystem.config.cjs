56b6d00c9ad181b29826eb81fc8518ffde0ef59e
Tomer Neeman <tomerneeman@Tomers-MacBook-Pro.local>
2026-02-06 14:05:38 +0200

    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,7 +12,21 @@ module.exports = {
             max_memory_restart: '300M',
             env: {
                 NODE_ENV: 'production',
-                PORT: 80
+                PORT: 3000
+            }
+        },
+        {
+            name: 'taskpoint-frontend',
+            script: 'npm',
+            args: 'run preview',
+            error_file: './logs/frontend-err.log',
+            out_file: './logs/frontend-out.log',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            env: {
+                NODE_ENV: 'production'
             }
         }
     ]
